055d51f1c871eefccd71299a72e0fae461d0824e
MatthiasJanssen <70886150+MatthiasJanssen@users.noreply.github.com>
2021-11-26 11:34:56 +0100

    removed methods from lightState Interface

--- implementation/BusinessLogic/src/main/java/api/LightState.java
+++ implementation/BusinessLogic/src/main/java/api/LightState.java
@@ -1,14 +1,6 @@
 package api;
 
 
-public abstract class LightState {
-
-    protected TrafficLight trafficLight;
-
-    public LightState(TrafficLight trafficLight){
-        this.trafficLight = trafficLight;
-    }
-
-    public abstract LightState changeState();
+public interface LightState {
 
 }
